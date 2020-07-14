719c3f40f9750115e5d81a2a1b308895d14f6549
Justin Clayton <jt.clayton92@yahoo.com>
2020-07-13 19:58:12 -0700

    Fix updating a show, add helper function for checking inputs on update/create

--- app/urls.py
+++ app/urls.py
@@ -6,6 +6,10 @@ app_name = 'app'
 urlpatterns = [
     path('', views.index, name='index'),
     path('shows/', views.shows, name='shows'),
+    path('shows/<int:show_id>/', views.view_show, name='view_show'),
+    path('shows/<int:show_id>/edit/', views.edit_show, name='edit_show'),
+    path('shows/<int:show_id>/update/', views.update_show, name='update_show'),
+    path('shows/<int:show_id>/delete/', views.delete_show, name='delete_show'),
     path('shows/new/', views.new_show, name='new_show'),
     path('shows/create/', views.create_show, name='create_show')
 ]
--- app/views.py
+++ app/views.py
@@ -1,19 +1,98 @@
 from django.shortcuts import render, redirect
 from django.urls import reverse
+from django.http import Http404
+from datetime import datetime
 
-# Create your views here.
+from app.models import Show
+
+
 def index(request):
   return redirect(reverse('app:shows'))
 
 def shows(request):
-  return render(request, 'shows.html')
+  context = {'shows': Show.objects.all()}
+  return render(request, 'shows.html', context)
+
+def view_show(request, show_id):
+  try:
+    context = {
+      'show': Show.objects.get(id=show_id)
+    }
+  except Show.DoesNotExist:
+    raise Http404('Show does not exist')
+  return render(request, 'view_show.html', context)
 
 def new_show(request):
-  return render(request, 'new_show.html')
+  context = {
+    'title': '',
+    'network': '',
+    'release_date': '',
+    'description': ''
+  }
+  return render(request, 'new_edit_show.html', context)
+
+def edit_show(request, show_id):
+  try:
+    show = Show.objects.get(id=show_id)
+    context = {
+      'title': show.title,
+      'network': show.network,
+      'release_date': show.release_date.strftime('%Y-%m-%d'),
+      'description': show.description.strip(),
+      'show_id': show.id
+    }
+  except Show.DoesNotExist:
+    raise Http404('Show not found')
+  return render(request, 'new_edit_show.html', context)
+
 
 def create_show(request):
+  if request.method == 'POST':
+    request, title, network, desc, release_date = check_inputs(request)
+
+    if not 'error-msg' in request.session:
+      new_show = Show.objects.create(
+        title=title,
+        network=network,
+        release_date=release_date,
+        description=desc
+      )
+      return redirect(reverse('app:view_show', args=(new_show.id,)))
+  return redirect(reverse('app:new_show'))
+
+def update_show(request, show_id):
+  if request.method == 'POST':
+    request, title, network, desc, release_date = check_inputs(request)
+
+    if not 'error-msg' in request.session:
+      try:
+        show = Show.objects.get(id=show_id)
+        show.title = title
+        show.network = network
+        show.release_date = release_date
+        show.description = desc
+        show.save()
+        return redirect(reverse('app:view_show', args=(show_id,)))
+      except Show.DoesNotExist:
+        raise Http404('Show not found')
+  return redirect(reverse('app:edit_show'), args=(show_id,))
+
+def delete_show(request, show_id):
+  return redirect('/')
+
+def check_inputs(request):
+  if 'error-msg' in request.session:
+    del request.session['error-msg']
   title = request.POST['title']
   network = request.POST['network']
-  release_date = request.POST['release_date']
   desc = request.POST['desc']
-  return redirect('/')
+  try:
+    release_date = datetime.strptime(
+      request.POST['release_date'], '%Y-%m-%d').date()
+  except Exception as e:
+    request.session['error-msg'] = 'Release date must be a date'
+  if len(title) < 2 or len(title) > 255:
+    request.session['error-msg'] = 'Title must be between 2 and 255 characters'
+  if len(network) < 2 or len(network) > 255:
+    request.session['error-msg'] = 'Network must be between 2 and 255 characters'
+  return (request, title, network, desc, release_date)
