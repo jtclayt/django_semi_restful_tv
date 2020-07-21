83c3b20e1f9eb24e6d5e646611c546bf0bb765ce
jtclayt <jt.clayton92@yahoo.com>
2020-07-20 18:40:00 -0700

    Fix flake8 errors, fix validator logic to account for new shows or editing shows with the same title.

--- app/models.py
+++ app/models.py
@@ -1,38 +1,41 @@
 from django.db import models
 from datetime import datetime
 
+
 class ShowManager(models.Manager):
-  def basicValidator(self, postData):
-    errors = {}
-    title = postData['title']
-    network = postData['network']
-    desc = postData['desc']
-    try:
-      release_date = datetime.strptime(
-        postData['release_date'], '%Y-%m-%d').date()
-    except ValueError:
-      errors['date'] = 'Release date must be a date'
-    if release_date > datetime.now().date():
-      errors['date'] = 'Release date must be in the past'
-    if len(title) < 2:
-      errors['title'] = 'Title must be 2 or more characters'
-    elif len(title) > 255:
-      errors['title'] = 'Title must be 255 or less characters'
-    elif len(Show.objects.filter(title=title)) > 0:
-      errors['title'] = 'Title already in database'
-    if len(network) < 2:
-      errors['network'] = 'Network must be 2 or more characters'
-    elif len(network) > 255:
-      errors['network'] = 'Network must be 255 or less characters'
-    if len(desc) == 0:
-      errors['desc'] = 'Must provide a description'
-    return errors
+    def basicValidator(self, postData, show_id=False):
+        errors = {}
+        title = postData['title']
+        network = postData['network']
+        desc = postData['desc']
+        try:
+            release_date = datetime.strptime(
+                postData['release_date'], '%Y-%m-%d').date()
+        except ValueError:
+            errors['date'] = 'Release date must be a date'
+        if release_date > datetime.now().date():
+            errors['date'] = 'Release date must be in the past'
+        if len(title) < 2:
+            errors['title'] = 'Title must be 2 or more characters'
+        elif len(title) > 255:
+            errors['title'] = 'Title must be 255 or less characters'
+        elif len(Show.objects.filter(title=title)) > 0:
+            if (not show_id) or (Show.objects.get(id=show_id).title != title):
+                errors['title'] = 'Title already in database'
+        if len(network) < 2:
+            errors['network'] = 'Network must be 2 or more characters'
+        elif len(network) > 255:
+            errors['network'] = 'Network must be 255 or less characters'
+        if len(desc) == 0:
+            errors['desc'] = 'Must provide a description'
+        return errors
+
 
 class Show(models.Model):
-  title = models.CharField(max_length=255)
-  network = models.CharField(max_length=255)
-  release_date = models.DateField()
-  description = models.TextField(null=True)
-  created_at = models.DateTimeField(auto_now_add=True)
-  updated_at = models.DateTimeField(auto_now=True)
-  objects = ShowManager()
+    title = models.CharField(max_length=255)
+    network = models.CharField(max_length=255)
+    release_date = models.DateField()
+    description = models.TextField(null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    objects = ShowManager()
--- app/views.py
+++ app/views.py
@@ -8,90 +8,96 @@ from app.models import Show
 
 
 def index(request):
-  return redirect(reverse('app:shows'))
+    return redirect(reverse('app:shows'))
+
 
 def shows(request):
-  context = {'shows': Show.objects.all()}
-  return render(request, 'shows.html', context)
+    context = {'shows': Show.objects.all()}
+    return render(request, 'shows.html', context)
+
 
 def view_show(request, show_id):
-  try:
-    context = {
-      'show': Show.objects.get(id=show_id)
-    }
-  except Show.DoesNotExist:
-    raise Http404('Show does not exist')
-  return render(request, 'view_show.html', context)
+    try:
+        context = {
+            'show': Show.objects.get(id=show_id)
+        }
+    except Show.DoesNotExist:
+        raise Http404('Show does not exist')
+    return render(request, 'view_show.html', context)
+
 
 def new_show(request):
-  context = {
-    'title': '',
-    'network': '',
-    'release_date': '',
-    'description': ''
-  }
-  return render(request, 'new_edit_show.html', context)
+    context = {
+        'title': '',
+        'network': '',
+        'release_date': '',
+        'description': ''
+    }
+    return render(request, 'new_edit_show.html', context)
+
 
 def edit_show(request, show_id):
-  try:
-    show = Show.objects.get(id=show_id)
-    context = {
-      'title': show.title,
-      'network': show.network,
-      'release_date': show.release_date.strftime('%Y-%m-%d'),
-      'description': show.description.strip(),
-      'show_id': show.id
-    }
-  except Show.DoesNotExist:
-    raise Http404('Show not found')
-  return render(request, 'new_edit_show.html', context)
+    try:
+        show = Show.objects.get(id=show_id)
+        context = {
+            'title': show.title,
+            'network': show.network,
+            'release_date': show.release_date.strftime('%Y-%m-%d'),
+            'description': show.description.strip(),
+            'show_id': show.id
+        }
+    except Show.DoesNotExist:
+        raise Http404('Show not found')
+    return render(request, 'new_edit_show.html', context)
 
 
 def create_show(request):
-  if request.method == 'POST':
-    errors = Show.objects.basicValidator(request.POST)
+    if request.method == 'POST':
+        errors = Show.objects.basicValidator(request.POST)
 
-    if len(errors) == 0:
-      title = request.POST['title'].title()
-      network = request.POST['network'].title()
-      desc = request.POST['desc'].capitalize()
-      release_date = datetime.strptime(
-        request.POST['release_date'], '%Y-%m-%d').date()
-      new_show = Show.objects.create(
-        title=title,
-        network=network,
-        release_date=release_date,
-        description=desc
-      )
-      return redirect(reverse('app:view_show', args=(new_show.id,)))
+        if len(errors) == 0:
+            title = request.POST['title'].title()
+            network = request.POST['network'].title()
+            desc = request.POST['desc'].capitalize()
+            release_date = datetime.strptime(
+                request.POST['release_date'], '%Y-%m-%d').date()
+            new_show = Show.objects.create(
+                title=title,
+                network=network,
+                release_date=release_date,
+                description=desc
+            )
+            return redirect(reverse('app:view_show', args=(new_show.id,)))
 
-    for key, value in errors.items():
-      messages.error(request, value)
-  return redirect(reverse('app:new_show'))
+        for key, value in errors.items():
+            messages.error(request, value)
+    return redirect(reverse('app:new_show'))
+
 
 def update_show(request, show_id):
-  if request.method == 'POST':
-    errors = Show.objects.basicValidator(request.POST)
+    if request.method == 'POST':
+        errors = Show.objects.basicValidator(request.POST, show_id)
 
-    if len(errors) == 0:
-      try:
-        show = Show.objects.get(id=show_id)
-      except Show.DoesNotExist:
-        raise Http404('Show not found')
-      show.title = request.POST['title'].title()
-      show.network = request.POST['network'].title()
-      show.description = request.POST['desc'].capitalize()
-      show.release_date = datetime.strptime(
-        request.POST['release_date'], '%Y-%m-%d').date()
-      show.save()
-      return redirect(reverse('app:view_show', args=(show_id,)))
-    for key, value in errors.items():
-      messages.error(request, value)
-  return redirect(reverse('app:edit_show'), args=(show_id,))
+        if len(errors) == 0:
+            try:
+                show = Show.objects.get(id=show_id)
+            except Show.DoesNotExist:
+                raise Http404('Show not found')
+            show.title = request.POST['title'].title()
+            show.network = request.POST['network'].title()
+            show.description = request.POST['desc'].capitalize()
+            show.release_date = datetime.strptime(
+                request.POST['release_date'], '%Y-%m-%d').date()
+            show.save()
+            return redirect(reverse('app:view_show', args=(show_id,)))
+        for key, value in errors.items():
+            messages.error(request, value)
+    return redirect(reverse('app:edit_show'), args=(show_id,))
+
 
 def destroy_show(request, show_id):
-  try:
-    Show.objects.get(id=show_id).delete()
-  except Show.DoesNotExist:
-    raise Http404('Show not found')
-  return redirect('/')
+    try:
+        Show.objects.get(id=show_id).delete()
+    except Show.DoesNotExist:
+        raise Http404('Show not found')
+    return redirect('/')
